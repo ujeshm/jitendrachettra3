5d43bcb15b74ffd5b7b6063bc698c11b57ffc488
sijan sakya <shreejan.shakya@alpastechnology.com>
2026-01-22 17:47:55 +0545

    chores: rename btn title

--- src/components/page-components/map/LmcMap.tsx
+++ src/components/page-components/map/LmcMap.tsx
@@ -23,7 +23,7 @@ const LmcMap = () => {
         onClick={handleSwitchData}
         className="absolute bottom-5 cursor-pointer right-5 z-50 rounded-lg bg-white px-4 py-2 font-semibold text-black shadow-md hover:bg-gray-100"
       >
-        Switch to {isLmc ? 'Birgunj ward data' : 'LMC ward'}
+        Switch to {isLmc ? 'Parsa Area 1(Birgunj)' : 'LMC ward'}
       </button>
     </div>
   );
