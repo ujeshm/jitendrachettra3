23f980638123e6d9e9058a9e0e1e879ae0fb97ae
SijanSakya <shakyasijan01@gmail.com>
2026-01-26 15:50:29 +0545

    rename: deprecyated file middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // const accessToken = request.cookies.get('lmcVotersUser')?.value;
   // const { pathname } = request.nextUrl;
 
